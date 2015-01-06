ca7d24cc3cad1cda5adc50102a67ee2be2d5bf15
jlburkhead <jlburkhead@ucdavis.edu>
2015-01-05 21:52:24 -0800

    autoload db

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var net = require('net');
 var http = require('http');
 
 var Datastore = require('nedb');
-var db = new Datastore(__dirname + 'data/climate.db');
+var db = new Datastore({
+  filename: __dirname + '/data/climate.db',
+  autoload: true
+});
 
 net.createServer(function(socket) {
 
@@ -31,7 +34,7 @@ http.createServer(function(req, res) {
     db.find({}).sort({ date: -1 }).limit(100).exec(function(err, docs) {
       if (err) {
         console.log(err);
-        res.writeHead(405);
+        res.writeHead(500);
         res.end({ error: err });
         return;
       }
